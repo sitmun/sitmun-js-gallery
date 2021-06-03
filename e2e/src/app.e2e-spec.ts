deb76af2e47743656680a271d0fe59879005932d
didac.alsina <Alsina6897>
2021-06-03 10:46:41 +0200

    Acabar de fer el rename a galeria-sitmun3

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('openlayers-angular app is running!');
+    expect(await page.getTitleText()).toEqual('galeria-sitmun3 app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/openlayers-angular'),
+      dir: require('path').join(__dirname, './coverage/galeria-sitmun3'),
       subdir: '.',
       reporters: [
         { type: 'html' },
